c1f510c6c9f948db33c2e899f141f9b3e37c73bd
Tushar <30565750+tushar5526@users.noreply.github.com>
2023-01-04 13:45:42 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3333;
   await app.startAllMicroservices();
-  await app.listen(`0.0.0.0:${port}`);
+  await app.listen(port, '0.0.0.0');
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
 bootstrap();
