9686dcf2e9a54500c800c6842893499a84cd1642
KDwevedi <kanav11dwevedi@gmail.com>
2023-02-20 16:01:23 +0530

    browser

--- src/credentials/credentials.service.ts
+++ src/credentials/credentials.service.ts
@@ -278,7 +278,9 @@ export class CredentialsService {
   }
 
   async saveHTMLToPDF (data : string, path: string){
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
     const page = await browser.newPage()
     console.log(data);
     await page.setContent(data, {waitUntil: 'domcontentloaded'});
